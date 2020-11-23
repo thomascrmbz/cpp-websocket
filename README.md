12301063960e4151a8079ac8f83d397d89909019
TrikThom <thomas@trikthom.com>
2020-11-23 18:52:53 +0100

    Updated example client

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ A C++ WebSocket library
 # Getting Started
 
 ```c++
-#include <websocket.h>
+#include <websocket_client.h>
 
 int main() {
 
-  WebSocket::Server server;
+  WebSocket::Client client("localhost");
 
-  server.listen(3000);
+  client.on_connection = [&](WebSocket::Connection * connection) {
+    connection->write("Hello World!");
+
+    connection->on_message = [connection](std::string message) {
+      std::cout << message << std::endl;
+    };
+  };
+
+  client.connect();
 
   return 0;
 }
--- example_client.cpp
+++ example_client.cpp
@@ -1,21 +1,14 @@
 #include <websocket_client.h>
 
-#include <iostream>
-#include <unistd.h>
-
 int main() {
 
   WebSocket::Client client("localhost");
 
   client.on_connection = [&](WebSocket::Connection * connection) {
-    uint8_t buffer[] = { 0x81, 0x0C, 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
-    connection->write(buffer, sizeof(buffer));
+    connection->write("Hello World!");
 
     connection->on_message = [connection](std::string message) {
       std::cout << message << std::endl;
-      sleep(1);
-      uint8_t buffer[] = { 0x81, 0x04, 'p', 'i', 'n', 'g' };
-      connection->write(buffer, sizeof(buffer));
     };
   };
 
